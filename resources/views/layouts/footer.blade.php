ec4a6395144977c71f13aabdd580c19daffa4651
Salevizos <73824426+Salevizos@users.noreply.github.com>
2020-11-04 19:08:21 +0200

    table create

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <style> 
+        .page-footer {
+            position: absolute;
+            bottom: 0px;
+        } 
+        </style>
+    </head>
+<body>
+
+<footer class="page-footer font-small blue">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
+</body>
+</html>
+
+  @yield('footer')
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layouts')
+
+@section('content')
+    <div class="container">
+        <h1>Posts!</h1>
+
+    </div>
+@endsection
